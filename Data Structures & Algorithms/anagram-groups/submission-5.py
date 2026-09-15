87fb2fc3ab266b21f575370eab5a52e5adc07052
Mohamed Mostafa <mmostafa.ibra21@gmail.com>
2026-09-15 23:53:43 +0300

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.py
@@ -0,0 +1,15 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        from collections import defaultdict, Counter
+
+        code_to_anagrams = defaultdict(list)
+        for string in strs:
+            # building code
+            chars_counter = [0]*26
+            for char in string:
+                chars_counter[ord(char) - ord('a')] += 1
+
+            code = tuple(chars_counter)
+            code_to_anagrams[code].append(string)
+        
+        return list(code_to_anagrams.values())
